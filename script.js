9a2b66b65718b1178b85b29b2201b41bd3e0d0e9
furshetta <44312127+furshetta@users.noreply.github.com>
2018-12-09 17:26:19 +0400

    giorgi

---
+++ script.js
@@ -0,0 +1,11 @@
+let p = prompt("Enter String");
+if (p.match("chanta")) {
+    alert("akrdaluli sityva: chanta !!");
+}
+if (p.match("marketingi")) {
+    alert("akrdzaluli sityva: marketingi !!");
+}
+if (p.match("virusi")) {
+    alert("akrdzaluli sityva: virusi");
+}
+alert("You Entered: " + p);
